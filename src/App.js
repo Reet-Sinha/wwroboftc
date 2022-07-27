5c61b6495ae54752633469fdd795c0710e2716cc
DrIronfist <reetsinha2006@gmail.com>
2022-07-27 10:22:16 -0500

    basic routing and ui changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ResponsiveDrawer from './Components/ResponsiveDrawer'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBAoXi53mr-t23VfAtqt1m92K7qmzNpAo4",
-  authDomain: "westwood-robotics-ftc.web.app",
-  databaseURL: "https://westwood-robotics-ftc-default-rtdb.firebaseio.com",
-  projectId: "westwood-robotics-ftc",
-  storageBucket: "westwood-robotics-ftc.appspot.com",
-  messagingSenderId: "130945678658",
-  appId: "1:130945678658:web:06a52bcfb6d92103c69cdb",
-  measurementId: "G-WKBDP0V72D"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React lol
-          what da heck
-        </a>
-        <h1>
- 
-      </h1>
-      </header>
+        <ResponsiveDrawer/>
+      
       
     </div>
   );
---
+++ src/Components/ResponsiveDrawer.js
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import CodeIcon from '@mui/icons-material/Code';
+import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
+import LinkIcon from '@mui/icons-material/Link';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
+
+
+const drawerWidth = 240;
+const keyPages = {
+    'Hardware':<SettingsIcon/>, 
+    'CAD/Design':<DesignServicesIcon/>, 
+    'Software': <CodeIcon/>, 
+    'Marketing/Outreach':<AlternateEmailOutlinedIcon/>
+}
+
+const extraPages = {
+    'Useful Links':<LinkIcon/>, 
+    'Schedule':<CalendarMonthIcon/>, 
+    'Awards':<EmojiEventsIcon/>
+}
+
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {Object.keys(keyPages).map((text, index) => (  
+          <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <ListItem key={text} disablePadding>
+               <ListItemIcon>
+                 {keyPages[text]}
+               </ListItemIcon>
+               <ListItemText primary={text} />
+            </ListItem>
+            
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>  
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {Object.keys(extraPages).map((text, index) => (
+            <Link to ='/wwroboftc/about'>    
+                <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            {extraPages[text]}
+                        </ListItemIcon>
+                    <ListItemText primary={text} />
+                    </ListItemButton>
+                </ListItem>
+            </Link>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <BrowserRouter>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          background:"#B45F06"
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Westwood Robotics FTC
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+        <Box
+            component='main'
+            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+            <Toolbar/>
+            <Routes>
+          <Route exact path = '/wwroboftc/'>
+            
+          </Route>
+          <Route exact path='/wwroboftc/about/' element={<div className="About">About</div>}/>
+          
+        </Routes>
+        </Box>
+        
+    </Box>
+    </BrowserRouter>
+  );
+}
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
